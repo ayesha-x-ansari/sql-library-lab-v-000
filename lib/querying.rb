31cfaefb2b5864ac6eb1d0cdb22886c5592a0530
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-14 03:57:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,14 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  SELECT authors.name, subgenres.name
+  FROM authors
+  INNER JOIN series
+  on authors.id = series.author_id
+  iNNER JOIN subgenres
+  ON series.subgenre_id = subgenres.id
+  GROUP BY authors.id;"
+
 end
 
 def select_series_title_with_most_human_characters
