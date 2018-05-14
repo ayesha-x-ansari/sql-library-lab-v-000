c37995e2d0fc5fee4bb599da14b23a6e0aa04989
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-14 04:02:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,9 +35,25 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
+  "SELECT series.title 
+  FROM series 
+  INNER JOIN characters 
+  ON series.id = characters.series_id 
+  WHERE characters.species = 'human' 
+  GROUP BY series.id 
+  ORDER BY COUNT(characters.species) DESC 
+  LIMIT 1;"
 
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(books.title) 
+   FROM characters 
+   INNER JOIN character_books 
+   ON character_books.character_id = characters.id 
+   JOIN books ON books.id = character_books.book_id 
+   GROUP BY characters.name 
+   ORDER BY COUNT(books.title) DESC;"
+ end
 
 end
