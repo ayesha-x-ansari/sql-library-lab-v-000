ea0a2291e393d0bb0031e313c3e2a50b4175469c
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-14 03:01:41 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE series (id INTEGER PRIMARY KEY,
                     title TEXT,
                     author_id INTEGER,
-                    sub_genre_id INTEGER);
+                    subgenre_id INTEGER);
 
 CREATE TABLE sub_genre (id INTEGER PRIMARY KEY,
                           name TEXT);
